01be44f895cc2a41c871ecdc47aeed5a9231c88f
dmitryS1666 <dsuschinsky@gmail.com>
2025-12-18 13:49:26 +0300

    fixed assets

--- app/layout.js
+++ app/layout.js
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
         {children}
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+          integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
           crossOrigin="anonymous"
           async
         ></script>
