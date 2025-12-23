3da1b9d640a3e73e0c05f8af795421d80df1a9ef
ZemliakovD <working.dev2020@gmail.com>
2025-12-23 06:44:22 +0300

    .

--- components/layout/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Header } from './Header.jsx';
-export { default as Footer } from './Footer.jsx';
-
--- components/providers/AuthProvider.jsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { AuthProvider as AuthProviderContext } from '@/context/AuthContext';
-
-export default function AuthProvider({ children }) {
-    return <AuthProviderContext>{children}</AuthProviderContext>;
-}
-
