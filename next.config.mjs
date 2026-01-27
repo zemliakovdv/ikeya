77b9bbf1425e5b6d62eec8ac960ff79b201dab39
dmitryS1666 <dsuschinsky@gmail.com>
2026-01-28 01:00:55 +0300

    Add Docker support and update GitHub Actions workflow
    - Create Dockerfile and docker-compose.yml for frontend and NPM
    - Update next.config.mjs to use standalone output
    - Update GitHub Actions to deploy using Docker Compose

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'standalone',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
